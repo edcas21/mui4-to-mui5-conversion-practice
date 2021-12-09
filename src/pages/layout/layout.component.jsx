f9f1e12d72aea7f65d6177134aa2576bfd8a7d54
Edgar <cardenasedgar123@gmail.com>
2021-12-08 19:57:00 -0500

    Completed appbar section

--- src/pages/layout/layout.component.jsx
+++ src/pages/layout/layout.component.jsx
@@ -10,7 +10,10 @@ import {
   ListItemIcon,
   ListItemText,
   Typography,
+  AppBar,
+  Toolbar,
 } from "@mui/material";
+import { format } from "date-fns";
 
 //icons
 import { AddCircleTwoTone, SubjectTwoTone } from "@mui/icons-material";
@@ -37,6 +40,15 @@ const Layout = ({ children }) => {
   return (
     <Box sx={layoutStyles.root}>
       {/* app bar */}
+      <AppBar sx={layoutStyles.appbar} elevation={0}>
+        <Toolbar>
+          {/* Will take all the horizontal space available to it, which is why it pushes the name all the way to the right */}
+          <Typography sx={{ flexGrow: 1 }}>
+            Today is {format(new Date(), "MMMM do Y")}
+          </Typography>
+          <Typography>Mario</Typography>
+        </Toolbar>
+      </AppBar>
       {/* side drawer */}
       <Drawer
         // Use the css API to override the styling of sub-components of mui components : https://mui.com/api/drawer/#css
@@ -48,12 +60,16 @@ const Layout = ({ children }) => {
         anchor="left"
       >
         <Box>
-          <Typography sx={layoutStyles.title} variant="h5">Ninja Notes</Typography>
+          <Typography sx={layoutStyles.title} variant="h5">
+            Ninja Notes
+          </Typography>
         </Box>
         <List>
           {menuItems.map((menuItem, index) => (
             <ListItem
-              sx={location.pathname === menuItem.path ? layoutStyles.active : null}
+              sx={
+                location.pathname === menuItem.path ? layoutStyles.active : null
+              }
               button
               key={index}
               onClick={() => navigate(menuItem.path)}
@@ -64,7 +80,10 @@ const Layout = ({ children }) => {
           ))}
         </List>
       </Drawer>
-      <Box sx={layoutStyles.page}>{children}</Box>
+      <Box sx={layoutStyles.page}>
+        <Box sx={{ ...layoutStyles.toolbar, marginTop: 2 }} />
+        {children}
+      </Box>
     </Box>
   );
 };
--- src/pages/layout/layout.styles.js
+++ src/pages/layout/layout.styles.js
@@ -3,26 +3,30 @@ import ourTheme from "../../assets/our-theme";
 const drawerWidth = 240;
 
 const layoutStyles = {
-    page: {
-        background: "#f9f9f9",
-        width: "100%",
-        padding: ourTheme.spacing(3),
-    },
-    drawer: {
-        width: drawerWidth,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    root: {
-        display: 'flex',
-    },
-    active: {
-        background: "#f4f4f4"
-    },
-    title: {
-        padding: ourTheme.spacing(2)
-    },
+  page: {
+    background: "#f9f9f9",
+    width: "100%",
+    padding: ourTheme.spacing(3),
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
+  },
+  active: {
+    background: "#f4f4f4",
+  },
+  title: {
+    padding: ourTheme.spacing(2),
+  },
+  appbar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  toolbar: ourTheme.mixins.toolbar,
 };
 
 export default layoutStyles;
